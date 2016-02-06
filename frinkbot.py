4a8419c9297fb7ee376262eb99c42632e2d207a2
Sam Milledge <sam@sammilledge.com>
2016-02-06 15:45:34 +1000

    Add an ephemeral response if the user's command cannot be parsed, or there's a sever error

--- frinkbot.py
+++ frinkbot.py
@@ -18,14 +18,17 @@ def index():
 @slack.command(command=settings.SLACK_COMMAND, token=settings.SLACK_COMMAND_TOKEN,
                team_id=settings.SLACK_TEAM_ID, methods=['POST'])
 def slash_frink(**kwargs):
-    meme_url = make_meme(*parse_command_text(kwargs.get('text')))
-    if meme_url:
-        return slack.response('', attachments=[{
-            'fallback': 'HOYVIN-GLAVIN!',
-            'image_url': meme_url
-        }], response_type='in_channel')
-    else:
-        return slack.response('Nothing found... Oh, for flavin out loud.', response_type='ephemeral')
+    try:
+        meme_url = make_meme(*parse_command_text(kwargs.get('text')))
+        if meme_url:
+            return slack.response('', attachments=[{
+                'fallback': 'HOYVIN-GLAVIN!',
+                'image_url': meme_url
+            }], response_type='in_channel')
+        else:
+            return slack.response('Nothing found! Oh, for flavin out loud.', response_type='ephemeral')
+    except:
+        return slack.response('Something exploded! Oh, for flavin out loud.', response_type='ephemeral')
 
 
 if __name__ == '__main__':
